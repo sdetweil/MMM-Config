74eff1d6880d057ac7067618a944b5b5e1eb7f88
sam detweiler <sdetweil@gmail.com>
2023-12-03 19:39:55 -0600

    change  name of module specific schema creattion tool

--- create_form_for_module.cmd
+++ create_form_for_module.cmd
@@ -1,3 +1,7 @@
 @rem
 set d=%~dp0
+if [%1]!=[] goto okparm
+   echo missing module name
+   exit 1
+:okparm 
 node  scripts\buildschema4.js %d%defaults.js saveform %1 >nul
--- create_form_for_module.sh
+++ create_form_for_module.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
+if [ "$1." == "." ]; then
+   echo module name missing
+   exit 1
+fi
 node  scripts/buildschema4.js ../defaults.js saveform $1 >/dev/null
