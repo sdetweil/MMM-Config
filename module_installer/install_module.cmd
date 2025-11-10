7e7d6915456157142290e1cef58e813268f847b0
same <ssdetweil@gmail.com>
2025-11-10 11:17:40 -0600

    fix installer script to handle requires missing from (missing)package.json

--- module_installer/install_module.cmd
+++ module_installer/install_module.cmd
@@ -1,5 +1,6 @@
 @echo off
-set known_list="request valid-url jsdom node-fetch digest-fetch"
+set known_list=" request valid-url jsdom node-fetch digest-fetch"
+set skip_list=" moment node_helper moment logger os path child_process fs"
 set modules_path=..\..\..\modules
 set module_name=%1
 set module_url=%2
@@ -19,18 +20,37 @@ rem     # we are in the module folder
 rem 
 rem     # if the node_helper exists
 	if exist "%mod%" (	
-		for /F "tokens=2 delims=()" %%i in ('findstr "require(" %mod%') do ( set "rn=%%~nxi"
+		for /F "tokens=2 delims=()" %%i in ('findstr "require(" %mod%') do ( 
+		    @set "rn=%%~nxi"
 			@rem echo rn=!rn!
-			Echo.!known_list!| findstr /C:"!rn!">nul && (
-				@rem Echo.Substring !rn! found!
-				@rem Echo.%%i
-				if not exist !keyfile! (
-				   npm init -y
-				)
-				if !rn! == "node-fetch" (
-					set rn="!rn!@2"
-				)
-				npm install !JustProd! !rn! >>!logfile! 2>&1	
+			@rem echo first !skip_list!
+			@rem check for require of MM supplied functions, doesn't need package.json dependency entry
+			@echo > .failed.tmp
+			@Echo !skip_list!| findstr /C:"!rn!" >nul && @del .failed.tmp
+			@echo off
+			@if exist .failed.tmp @(			
+				@del .failed.tmp
+				@rem echo !rn! not found in list
+					@rem Echo.%%i
+					@set xxx=!rn!
+					@if not exist !keyfile! (
+					   @echo creating !keyfile!
+					   @npm init -y 2>&1 >>!logfile! 
+					   @ echo . >nul
+					   @set rn=!xxx!
+					   @echo !keyfile! created
+					)
+					@if !xxx! == "node-fetch" (
+						@set xxx="!rn!@2"
+					)
+					@echo adding !xxx! to module 
+					@echo npm install !JustProd! !xxx! 2>&1 >>!logfile!
+					@if !xxx! == "" echo library name missing
+					@npm install !JustProd! !xxx! 2>&1 
+					@rem !logfile!
+					@rem echo installing done
+			) else (
+			   @rem echo !rn! found in !skip_list!
 			)
 		)
 	)
@@ -41,13 +61,13 @@ rem     # if the node_helper exists
 		echo "received request to install module " %module_name% from %module_url%
 		if not exist %module_name% (
 			git clone %module_url% >%logfile% 2>&1
-			move %logfile% %module_name%
+			move %logfile% %module_name% >nul
 		)
 		cd %module_name%
 		if exist %mod% (
 			call :check_update_dependencies			
 			if exist %keyfile% (
-			    for /F "usebackq" %%i IN (`findstr "dependencies" !keyfile! ^| find /c /v ""`) do set results=%%i
+			    for /F "usebackq" %%i IN (`findstr "dependencies" !keyfile! ^| find /c /v ""`) do @set results=%%i
 				if !results! equ 1 (
 					npm install %JustProd% 2>&1 >>%logfile%
 				)
